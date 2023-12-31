d60dd25714f4350e148835eb759312f04bd953a0
Yesaya Jones <yesayajones@gmail.com>
2023-12-31 10:55:29 +0200

    chore: Add routes and views

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require ('express')
+const articleRouter = require('./routes/articles')
 const app = express()
 
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+
 app.get('/', (req, res) => {
-    res.send('Hello, world')
+    const articles = [{
+        title: 'Test Article 1',
+        createdAt: new Date(), 
+        description: 'Test description 1'
+    },
+    {
+        title: 'Test Article  2',
+        createdAt: new Date(), 
+        description: 'Test description 2'
+    }]
+    res.render('index', {articles: articles})
 })
 
 app.listen(5000)
