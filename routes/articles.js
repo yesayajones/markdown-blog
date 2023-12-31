5291ce91127e81a96bf18ce38dca91e853aaa292
Yesaya Jones <yesayajones@gmail.com>
2023-12-31 11:48:07 +0200

    chore: Create form

--- routes/articles.js
+++ routes/articles.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-    res.send('In articles')
+    res.render('articles/new')
+})
+
+router.post('/', (req, res) => {
+    
 })
 
 module.exports = router
