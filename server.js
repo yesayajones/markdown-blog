519a0c0be75c21eca7b879f0f144f4d90a5734f7
Yesaya Jones <yesayajones@gmail.com>
2024-01-05 14:52:25 +0200

    chore: Add the delete button and method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require ('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb://localhost:27017/blog');
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false}))
+app.use(methodOverride('_method'))
 
 
 
